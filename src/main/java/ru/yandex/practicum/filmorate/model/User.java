530912a41a581e553e92a49e0f87d6484884f714
ZhiRafik <roketro009@gmail.com>
2024-12-17 16:41:07 +0300

    Убрать лишнюю аннотацию

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import java.time.LocalDate;
-import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class User {
     long id;
-    @Email
     String email;
     String login;
     String name;
